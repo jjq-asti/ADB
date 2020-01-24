4c5aac27c51cbd621fb78ecc3245f801c97e3806
jay <robot-1>
2020-01-24 08:58:33 +0800

    fixed bug not getting coords

--- ADB.py
+++ ADB.py
@@ -4,7 +4,22 @@ import re
 def getRawGpsCoordinates():
     process = subprocess.Popen(['adb', 'shell', 'dumpsys', 'location', '|', 'grep', 'Longitude'],stdout=subprocess.PIPE, stderr= subprocess.PIPE)
     stdout,stderr = process.communicate()
-    return stdout,stderr
+    if not stdout:
+        process = subprocess.Popen(['adb', 'shell', 'dumpsys', 'location', '|', 'grep', '"network: Location"'],stdout=subprocess.PIPE, stderr= subprocess.PIPE)
+        stdout,stderr = process.communicate()
+        if not stdout:
+            return None
+        data = stdout.decode().split()[2].split(',')
+        return data
+    if not stderr:
+        data = stdout.decode().split(',')
+        data = data[0:2]
+        data = "".join(data)
+        values = re.findall('\d+\.\d+',data)
+        if not values:
+            return None
+        return values
+    return None
 
 def parseGpsCoordinates(raw_gps_coordinates):
     data = raw_gps_coordinates
@@ -19,5 +34,5 @@ def parseGpsCoordinates(raw_gps_coordinates):
 
 if __name__ == "__main__":
     raw_data = getRawGpsCoordinates()
-    data = parseGpsCoordinates(raw_data)
-    print(data)
+    # data = parseGpsCoordinates(raw_data)
+    print(raw_data)
